6aff2f7bfec2b2422dfcb46df353a6db188bd702
Rybintseva <rybintseva.t@gmail.com>
2017-07-02 01:20:29 +0300

    Some functions added

--- sandbox/src/main/java/test/sandbox/First.java
+++ sandbox/src/main/java/test/sandbox/First.java
@@ -2,7 +2,26 @@ package test.sandbox;
 
 public class First {
 
-	public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
- }
+  public static void main(String[] args) {
+    hello("world");
+    hello("Tatiana");
+
+    double len = 5;
+    System.out.println("The area of square is " + area(len));
+
+    double a = 7;
+    double b = 98;
+    System.out.println("The area of rectangle is " + area(a, b));
+  }
+  public static void hello(String user) {
+    System.out.println("Hello, " + user + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
